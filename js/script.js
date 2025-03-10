3090c2ee9731050c4691ae0abfe09eb63ac72af2
N1k25 <grushko_nikolai@ukr.net>
2025-03-10 11:45:25 +0200

    Create section Reviews

--- js/script.js
+++ js/script.js
@@ -19,5 +19,25 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.slider-2').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:3,
+		autoplay:false,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
 
